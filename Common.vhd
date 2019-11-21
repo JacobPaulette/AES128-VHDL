6df1c14062644249b3692867cac8d13a4e0c9a40
JacobPaulette <jacobpaulette@gmail.com>
2019-11-21 12:38:44 -0500

    Added RoundKeys type

--- Common.vhd
+++ Common.vhd
@@ -7,6 +7,7 @@ package Common is
     subtype BYTE is std_logic_vector(7 downto 0);
     type column is array (0 to 3) of BYTE;
     type state_byte is array (0 to 15) of BYTE;
+    type RoundKeys is array (0 to 11) of state_byte;
     type state is array (0 to 3) of column;
 
 end Common;
