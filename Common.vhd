eb7f08fca1cc5dd6599e5320aada342839132258
Jacob Paulette <jacobpaulette@gmail.com>
2019-12-03 22:14:26 -0500

    off by 1

--- Common.vhd
+++ Common.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 
 package Common is
     subtype BYTE is std_logic_vector(7 downto 0);
-    subtype RoundKey is std_logic_vector(128 downto 0);
+    subtype RoundKey is std_logic_vector(127 downto 0);
     type column is array (0 to 3) of BYTE;
     type state_byte is array (0 to 15) of BYTE;
     type RoundKeys is array (0 to 10) of state_byte;
